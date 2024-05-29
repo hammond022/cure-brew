3b31ca66988c90b09832527c04bf8a672d8c1e5c
SteamyRize <AnimalClubBartBear@gmail.com>
2024-05-29 18:22:34 +0800

    finished individual html but unfinished recommendations on each html

---
+++ eachprodhtml/my2.js
@@ -0,0 +1,79 @@
+document.getElementById("drink1").onclick = function () {
+    location.href = "Whipped Drizzled Choco.html";
+  };
+
+  document.getElementById("drink2").onclick = function () {
+    location.href = "Chocolate Drink.html";
+  };
+
+  document.getElementById("drink3").onclick = function () {
+    location.href = "Double Chocolate Chip.html";
+  };
+
+  document.getElementById("drink4").onclick = function () {
+    location.href = "Ice Mocha.html";
+  };
+
+  document.getElementById("drink5").onclick = function () {
+    location.href = "Ice Americano.html";
+  };
+
+  document.getElementById("drink6").onclick = function () {
+    location.href = "eachprodhtml/Salted Caramel.html";
+  };
+
+  document.getElementById("pastry1").onclick = function () {
+    location.href = "Strawberry Tart.html";
+  };
+
+  document.getElementById("pastry2").onclick = function () {
+    location.href = "Raspberry Cheesecake.html";
+  };
+
+  document.getElementById("pastry3").onclick = function () {
+    location.href = "Chocolate Stick Bread.html";
+  };
+
+  document.getElementById("pastry4").onclick = function () {
+    location.href = "Croissant.html";
+  };
+
+  document.getElementById("pastry5").onclick = function () {
+    location.href = "Classic Brownie.html";
+  };
+
+  document.getElementById("pastry6").onclick = function () {
+    location.href = "Chocolate Chip Brownie.html";
+  };
+
+  document.getElementById("pastry7").onclick = function () {
+    location.href = "Raspberry Brownie.html";
+  };
+
+  document.getElementById("pastry8").onclick = function () {
+    location.href = "Ice Cream Brownie.html";
+  };
+
+  document.getElementById("drink7").onclick = function () {
+    location.href = "Mango Tapioca.html";
+  };
+
+  document.getElementById("drink8").onclick = function () {
+    location.href = "Milo.html";
+  };
+
+  document.getElementById("drink9").onclick = function () {
+    location.href = "Watermelon Soda.html";
+  };
+
+  document.getElementById("drink10").onclick = function () {
+    location.href = "Lemon Soda.html";
+  };
+
+  document.getElementById("drink11").onclick = function () {
+    location.href = "S'mores Drink.html";
+  };
+
+  document.getElementById("drink12").onclick = function () {
+    location.href = "Strawberry Shake.html";
+  };
